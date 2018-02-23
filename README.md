089b9a62342ff14b227fea34cdf41438ccebdb5e
Steven G. Johnson <stevenj@mit.edu>
2018-02-23 17:51:43 -0500

    initial revision

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 [![Coverage Status](https://coveralls.io/repos/stevengj/VersionParsing.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/stevengj/VersionParsing.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/stevengj/VersionParsing.jl/coverage.svg?branch=master)](http://codecov.io/github/stevengj/VersionParsing.jl?branch=master)
+
+The `VersionParsing` package implements flexible parsing of
+version-number strings into Julia's built-in `VersionNumber` type, via
+the `vparse(string)` function.
+
+Unlike the `VersionNumber(string)` constructor, `vparse(string)` can
+handle version-number strings in a much wider range of formats.
--- src/VersionParsing.jl
+++ src/VersionParsing.jl
@@ -1,5 +1,38 @@
-module VersionParsing
+"""
+The `VersionParsing` package implements flexible parsing of
+version-number strings into Julia's built-in `VersionNumber` type, via
+the `vparse(string)` function.
 
-# package code goes here
+Unlike the `VersionNumber(string)` constructor, `vparse(string)` can
+handle version-number strings in a much wider range of formats.
+"""
+module VersionParsing
+export vparse
+
+"""
+    vparse(s::AbstractString)
+
+Returns a `VersionNumber` representing the version information in the
+string `s`.
+
+Typically, `s` is a string of the form `"major.minor.patch"`, but many
+variations on this format are supported, including:
+
+* `"major.minor.patch[.+-]something[.+-]something"` is supported, converted into the
+  closest `VersionNumber` equivalent (`something` becomes a prerelease
+  or build identifier).
+* `"version"` or `"v"` prefixes (of any case) are stripped.
+* All whitespace is ignored.
+"""
+vparse(s::AbstractString) = vparse(String(s))
+
+function vparse(s::String)
+    s = replace(s, isspace=>"") # rm whitespace
+    s = replace(s, r"^(version|vers|v)"i=>"") # strip prefix
+    try
+        return VersionNumber(s)
+    catch
+    end
+end
 
 end # module
